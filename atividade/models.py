e16a1b4c95c0945fce9701fd02206af3845fc532
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-07-16 19:12:31 -0500

    feat: init atividade model

---
+++ atividade/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Atividade(models.Model):
+
+    CHOICES = (
+        (1, 'Baixa'),
+        (2, 'Média'),
+        (3, 'Alta'),
+    )
+
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    dataInicio = models.DateField()
+    dataFim = models.DateField()
+    status = models.BooleanField()
+    prioridade = models.IntegerChoices(choices=CHOICES)
+
+    def __str__(self):
+        return self.nome
